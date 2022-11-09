9b75edcdc003c1c24b5ed8b3cb4eecafcd1465c9
Ani Mihaylova <anivm.webdev@gmail.com>
2022-11-09 05:44:15 -0500

    update knex variables

--- knexfile.js
+++ knexfile.js
@@ -3,12 +3,12 @@ require('dotenv').config();
 module.exports = {
   client: "mysql",
   connection: {
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    url: process.env.DB_URL,
+    host: process.env.DB_HOST || MYSQLHOST,
+    database: process.env.DB_DATABASE || MYSQLDATABASE,
+    user: process.env.DB_USERNAME || MYSQLUSER,
+    password: process.env.DB_PASSWORD || MYSQLPASSWORD,
+    port: process.env.DB_PORT || MYSQLPORT,
+    url: process.env.DB_URL || MYSQL_URL,
     charset: "utf8"
   }
 };
