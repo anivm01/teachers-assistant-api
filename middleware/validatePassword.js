10e9fced07717f91474b5f5f536686775b2ffaf3
Ani Mihaylova <anivm.webdev@gmail.com>
2022-11-20 12:10:33 -0500

    add encrypt and validate password middlewares for user routes

---
+++ middleware/validatePassword.js
@@ -0,0 +1,26 @@
+const bcrypt = require("bcrypt");
+const knex = require('knex')(require("../knexfile"));
+
+
+module.exports = async (req, res, next) => {
+    if (!req.body.email || !req.body.password){
+        return res.status(400).json({error:"Login requires username and password fields"})
+     }
+    try {
+        const foundUsers = await knex ('user')
+                            .select("*")
+                            .where({email: req.body.email})
+
+        if (foundUsers.length === 0) {
+            return res.status(401).json({error:"invalid login credentials"})
+        }
+        const passwordIsValid = await bcrypt.compare(req.body.password, foundUsers[0].password)
+        if(!passwordIsValid){
+            return res.status(401).json({error:"incorrect password"})
+        }
+    }
+    catch (err) {
+        return res.status(500).json({message:"There was an error", error: err})
+    }
+    next();
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,11 +3,12 @@ const knex = require('knex')(require("../knexfile"));
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const passwordHash = require('../middleware/passwordHash');
+const encryptPassword = require('../middleware/encryptPassword');
+const validatePassword = require('../middleware/validatePassword')
 
 
 router
-.post('/signup', passwordHash, async (req, res) => {
+.post('/signup', encryptPassword, async (req, res) => {
     try { 
         if(!req.body.name || !req.body.email || !req.body.password) {
             return res.status(400).json({status:400, message:"Please make sure to provide a name, an email and a password"})
@@ -26,36 +27,20 @@ router
         res.status(500).json({message:"There was an error with the database", error: err})
     }
 })
-.post("/login",  async (req, res) => {
-    
-    const { email, password } = req.body
-    
-    if (!email || !password){
-       return res.status(400).json({error:"Login requires username and password fields"})
-    }
-  
-    const foundUsers = await knex
-                            .select("*")
-                            .from("user")
-                            .where({email: email})
+.post("/login", validatePassword,  async (req, res) => {
+    try{
+        const foundUsers = await knex
+        .select("user.id")
+        .from("user")
+        .where({email: req.body.email})
 
-    if (foundUsers.length === 0) {
-        return res.status(401).json({error:"invalid login credentials"})
+        const token = jwt.sign({ user_id: foundUsers[0] }, process.env.JWT_SECRET_KEY);
+        
+        res.status(200).json({ message: "Successfully logged in", token: token })
     }
-
-    const user = foundUsers[0]
-    let passwordIsValid = false; 
-    try {
-       passwordIsValid = await bcrypt.compare(password, user.password)
+    catch (err) {
+        res.status(500).json({message:"There was an error with the server", error: err})
     }
-    catch{
-        return res.status(500).json({error:"server error"})
-    }
-    if(!passwordIsValid){
-        return res.status(401).json({error:"incorrect password"})
-    }
-    const token = jwt.sign({ user_id: user.id }, process.env.JWT_SECRET_KEY);
-    res.json({ message: "Successfully logged in", token: token })
 })
 
 
